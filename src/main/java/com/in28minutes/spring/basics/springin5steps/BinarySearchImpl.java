fc83257090882e522ab5492d5dac6169c5a4cf20
Luke Guan <lukeguan@outlook.com>
2022-03-03 23:47:53 -0800

    first commit

---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,18 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+public class BinarySearchImpl {
+	private SortAlgorithm sortAlgorithm;
+	
+	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+		super();
+		this.sortAlgorithm = sortAlgorithm;
+	}
+
+	public int binarySearch(int[] numbers, int numberToSearchFor) {
+		
+		int[] sortedNumbers = sortAlgorithm.sort(numbers);
+		System.out.println(sortAlgorithm);
+		
+		return 3;
+	}
+}
