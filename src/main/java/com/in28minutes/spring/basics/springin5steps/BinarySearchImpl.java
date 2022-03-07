d13c4c56166577393250192f2090d877e1a863e9
Luke Guan <lukeguan@outlook.com>
2022-03-06 22:32:53 -0800

    bean

--- src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,6 +1,12 @@
 package com.in28minutes.spring.basics.springin5steps;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BinarySearchImpl {
+	
+	@Autowired // tell binary search impl depends on sort algorithm
 	private SortAlgorithm sortAlgorithm;
 	
 	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
--- src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,16 +2,28 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
+	
+	// what are the beans? add comp
+	// what are the dependencies of a bean? add autowired
+	// where to search for beans?
+	
+	
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
-		int result = binarySearch.binarySearch(new int[] {123,4,5}, 3);
-		System.out.println(result);
+//		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+				
+		ApplicationContext applicationContext = 
+				SpringApplication.run(SpringIn5StepsApplication.class, args);
 		
-		SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+		int result = binarySearch.binarySearch(new int[] {123,4,5}, 3);
+		
+		System.out.print(result);
 	}
 
 }
