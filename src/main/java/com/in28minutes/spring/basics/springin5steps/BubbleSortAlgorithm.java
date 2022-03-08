254959886b305dff052211f2d84218f6359977c1
Luke Guan <lukeguan@outlook.com>
2022-03-07 22:17:37 -0800

    primary

--- src/main/java/com/in28minutes/spring/basics/springin5steps/BubbleSortAlgorithm.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -3,6 +3,7 @@ package com.in28minutes.spring.basics.springin5steps;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm {
 	public int[] sort(int[] numbers) {
 		return numbers;
--- src/main/java/com/in28minutes/spring/basics/springin5steps/QuickSortAlgorithm.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -1,5 +1,7 @@
 package com.in28minutes.spring.basics.springin5steps;
 
+
+@Component
 public class QuickSortAlgorithm implements SortAlgorithm{
 	public int[] sort(int[] numbers) {
 		return numbers;
