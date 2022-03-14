80b87ebeb6a2e57db56e3c73c04f9cc3dd7970c0
Luke Guan <lukeguan@outlook.com>
2022-03-13 22:51:40 -0700

    post construct predestory

--- src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,10 +1,18 @@
 package com.in28minutes.spring.basics.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	
 	@Autowired // tell binary search impl depends on sort algorithm
 	private SortAlgorithm quickSortAlgorithm;
@@ -20,5 +28,15 @@ public class BinarySearchImpl {
 		System.out.println(quickSortAlgorithm);
 		
 		return 3;
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		logger.info("postConstruct");
+	}
+	
+	@PreDestroy
+	public void preDestroy() {
+		logger.info("@PreDestroy");
 	}
 }
