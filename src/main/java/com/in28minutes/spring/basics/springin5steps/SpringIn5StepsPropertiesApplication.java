17437e726f5f3255a55aa9c1cd9394b515a21844
Luke Guan <lukeguan@outlook.com>
2022-03-17 00:41:18 -0700

    read from external file

---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsPropertiesApplication.java
@@ -0,0 +1,32 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+//import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
+import com.in28minutes.spring.basics.springin5steps.properties.SomeExternalService;
+
+@Configuration
+@ComponentScan("com.in28minutes.spring.basics.springin5steps")
+//app.properties
+@PropertySource("classpath:app.properties")
+public class SpringIn5StepsPropertiesApplication {
+
+	public static void main(String[] args) {
+				
+		try(AnnotationConfigApplicationContext applicationContext = 
+				new AnnotationConfigApplicationContext(SpringIn5StepsPropertiesApplication.class)){
+			
+			SomeExternalService service = applicationContext.getBean(SomeExternalService.class);
+			
+			System.out.print(service);
+		}
+		
+
+	}
+
+}
