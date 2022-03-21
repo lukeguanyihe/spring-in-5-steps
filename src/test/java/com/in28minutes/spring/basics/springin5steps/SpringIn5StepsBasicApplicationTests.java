4c2cb0b481e6df6370cd09d923540b23c50e5053
Luke Guan <lukeguan@outlook.com>
2022-03-21 00:16:23 -0700

    spring unit with java context

--- src/test/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplicationTests.java
+++ src/test/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplicationTests.java
@@ -1,10 +1,16 @@
 package com.in28minutes.spring.basics.springin5steps;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class SpringIn5StepsBasicApplicationTests {
+//@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class SpringIn5StepsBasicApplicationTests {
 
 	@Test
 	void contextLoads() {
